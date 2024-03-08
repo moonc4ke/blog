85bdedf800d9239713cfa5b3f999fc5afb3398d9
mooncake <adom.donatas@gmail.com>
2024-03-08 18:16:06 +0800

    Update tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,42 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
 
-module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        rosePine: {
-          base: '#191724',
-          surface: '#1f1d2e',
-          overlay: '#26233a',
-          muted: '#6e6a86',
-          subtle: '#908caa',
-          text: '#e0def4',
-          love: '#eb6f92',
-          gold: '#f6c177',
-          rose: '#ebbcba',
-          pine: '#31748f',
-          foam: '#9ccfd8',
-          iris: '#c4a7e7',
-          highlightLow: '#21202e',
-          highlightMed: '#403d52',
-          highlightHigh: '#524f67',
-        },
+export const content = [
+  './public/*.html',
+  './app/helpers/**/*.rb',
+  './app/javascript/**/*.js',
+  './app/views/**/*.{erb,haml,html,slim}',
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ..._fontFamily.sans],
+    },
+    colors: {
+      rosePine: {
+        base: '#191724',
+        surface: '#1f1d2e',
+        overlay: '#26233a',
+        muted: '#6e6a86',
+        subtle: '#908caa',
+        text: '#e0def4',
+        love: '#eb6f92',
+        gold: '#f6c177',
+        rose: '#ebbcba',
+        pine: '#31748f',
+        foam: '#9ccfd8',
+        iris: '#c4a7e7',
+        highlightLow: '#21202e',
+        highlightMed: '#403d52',
+        highlightHigh: '#524f67',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ],
 };
+export const plugins = [
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/aspect-ratio'),
+  require('@tailwindcss/typography'),
+  require('@tailwindcss/container-queries'),
+];
